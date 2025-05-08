2898b551ebda4dc8afa5a73b532c1006e182b5d4
Maria Clara Ribeiro de Brito <20221101110014@ifrn.local>
2025-05-08 13:41:20 -0300

    nada

---
+++ aula1/src/MyFirstComponent.jsx
@@ -0,0 +1,12 @@
+function MyFirstComponent(){
+    const nome = "carange"
+
+    return (
+        <div>
+            <h1>HELLO WORLD</h1>
+            <a href = "https://instagram.com/cllararibeiro"><h2>{nome}</h2></a>
+        </div>
+    )
+}
+
+export default MyFirstComponent
