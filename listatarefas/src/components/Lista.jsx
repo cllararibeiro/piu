fa90324a56ff18b582fe81f104465aae0c76aecb
Maria Clara <brito.clara@escolar.ifrn.edu.br>
2025-06-26 14:06:43 -0300

    lista tarefas

---
+++ listatarefas/src/components/Lista.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+export default function Lista(){
+
+    const [tarefa, setTarefa] = useState('')
+    const [lista, setLista]  = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLista([...lista, tarefa])
+        setTarefa('')
+
+    }
+
+    return(
+
+        <div>
+            <h2>Lista de Tarefas React</h2>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input type="text" name="tarefa" value={tarefa} onChange={(e) => setTarefa(e.target.value)} />
+                </label>
+                    <input type="submit" value="Adicionar" />
+
+            </form>
+
+            <ul>
+                {lista.map((item) =>
+                <li>{item}</li>)}
+            </ul>
+
+        </div>
+
+    )
+}
